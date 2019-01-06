85d055cec9219d5841776d319504790a176e24b6
moisesmcardona <moises@moises-studios.com>
2019-01-05 20:07:03 -0500

    Refactored Compression Finished Message

--- PerfectCompress/ExtraCode/CompressionFinishedMessage.vb
+++ PerfectCompress/ExtraCode/CompressionFinishedMessage.vb
@@ -7,12 +7,9 @@
             Dim OFSize As String = String.Empty
             If My.Computer.FileSystem.FileExists(Input) Then
                 OFSize = My.Computer.FileSystem.GetFileInfo(Input).Length
+                MsgBox("File Compression finished!" & Environment.NewLine & "Original File Size is " & OFSize & " bytes." & Environment.NewLine & "Compressed File Size is " & CFSize.Length & " bytes." & Environment.NewLine & "Compression Ratio is " & CFSize.Length / OFSize * 100 & "%")
             Else
                 OFSize = GetFolderSize(Input)
-            End If
-            If My.Computer.FileSystem.FileExists(Input) Then
-                MsgBox("File Compression finished!" & Environment.NewLine & "Original File Size is " & OFSize & " bytes." & Environment.NewLine & "Compressed File Size is " & CFSize.Length & " bytes." & Environment.NewLine & "Compression Ratio is " & CFSize.Length / OFSize * 100 & "%")
-            ElseIf My.Computer.FileSystem.DirectoryExists(Input) Then
                 MsgBox("Folder Compression finished!" & Environment.NewLine & "Original Folder Size is " & OFSize & " bytes." & Environment.NewLine & "Compressed File Size is " & CFSize.Length & " bytes." & Environment.NewLine & "Compression Ratio is " & CFSize.Length / OFSize * 100 & "%")
             End If
         Catch
